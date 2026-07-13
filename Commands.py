81b646bebfac3953d1800f6a1962a4b0548688a3
it's Mugendi <148942143+collo-ctr@users.noreply.github.com>
2026-07-13 12:15:04 +0300

    Update Commands.py

--- Commands.py
+++ Commands.py
@@ -1,3 +1,12 @@
 
 git clone https://github.com/sirrom/Capstone-Project-Intelligent-Healthcare-Diagnostic-Assistant.git
 cd ai-capstone-healthcare
+
+
+# Windows:
+python -m venv venv
+venv\Scripts\activate
+
+# Mac/Linux:
+python -m venv venv
+source venv/bin/activate
